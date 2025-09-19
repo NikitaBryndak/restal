9394a6db8aa75f100a21848db04e1938a0d5c445
Nikita Bryndak <nikitabryndak@gmail.com>
2025-09-19 12:46:04 +0100

    loader Fix

--- components/ui/loader.tsx
+++ components/ui/loader.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { motion } from "motion/react";
+import { motion, easeInOut } from "motion/react";
 import React from "react";
 
 export const LoaderOne = () => {
@@ -9,7 +9,7 @@ export const LoaderOne = () => {
       repeat: Infinity,
       repeatType: "loop" as const,
       delay: x * 0.2,
-      ease: "easeInOut",
+      ease: easeInOut,
     };
   };
   return (
@@ -49,19 +49,19 @@ export const LoaderOne = () => {
 };
 
 export const LoaderTwo = () => {
-  const transition = (x: number) => {
+  const transitionTwo = (x: number) => {
     return {
       duration: 2,
       repeat: Infinity,
       repeatType: "loop" as const,
       delay: x * 0.2,
-      ease: "easeInOut",
+      ease: easeInOut,
     };
   };
   return (
     <div className="flex items-center">
       <motion.div
-        transition={transition(0)}
+        transition={transitionTwo(0)}
         initial={{
           x: 0,
         }}
@@ -77,7 +77,7 @@ export const LoaderTwo = () => {
         animate={{
           x: [0, 20, 0],
         }}
-        transition={transition(0.4)}
+        transition={transitionTwo(0.4)}
         className="h-4 w-4 -translate-x-2 rounded-full bg-neutral-200 shadow-md dark:bg-neutral-500"
       />
       <motion.div
@@ -87,7 +87,7 @@ export const LoaderTwo = () => {
         animate={{
           x: [0, 20, 0],
         }}
-        transition={transition(0.8)}
+        transition={transitionTwo(0.8)}
         className="h-4 w-4 -translate-x-4 rounded-full bg-neutral-200 shadow-md dark:bg-neutral-500"
       />
     </div>
@@ -129,7 +129,7 @@ export const LoaderFour = ({ text = "Loading..." }: { text?: string }) => {
     <div className="relative font-bold text-black [perspective:1000px] dark:text-white">
       <motion.span
         animate={{
-          skew: [0, -40, 0],
+          rotate: [0, -40, 0],
           scaleX: [1, 2, 1],
         }}
         transition={{
