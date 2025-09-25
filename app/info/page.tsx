6a3ed4117d67b13ee04ded94fc49472439d0d093
ElizabethCherrie <leobetta77@gmail.com>
2025-09-25 15:17:59 +0100

    initial commit

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -3,8 +3,7 @@ export default function InfoPage() {
   return (
       <>
         <div className="min-h-screen flex flex-col items-center justify-center px-4">
-            <h1 className="text-3xl font-light mb-3">Info Page</h1>
-            <p className="text-sm text-foreground/60">Work in progress...</p>
+            
         </div>
       </>
   );
