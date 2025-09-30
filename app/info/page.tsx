2c783b966914b93fae48271881fed3b9617722a1
ElizabethCherrie <leobetta77@gmail.com>
2025-09-30 21:45:16 +0100

    added baSIC LAYOUT

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -1,9 +1,19 @@
-
+import { Button } from "@/components/ui/button";
 export default function InfoPage() {
   return (
       <>
         <div className="min-h-screen flex flex-col items-center justify-center px-4">
-            
+            <h1>GG</h1>
+            <Button variant="">hello</Button>
+            <button
+              href="/3d-models"
+              className="px-6 py-4 text-black transition duration-100 bg-white border-2 border-black hover:bg-black hover:text-white"
+            >
+              Browse Models
+            </button>
+            <button></button>
+            <button></button>
+            <button></button>
         </div>
       </>
   );
