0b15ea242bac4fda1aae4aaac0319824771957cd
Nikita Bryndak <nikitabryndak@gmail.com>
2025-10-11 14:36:54 +0100

    fix

--- app/dashboard/trips/[id]/page.tsx
+++ app/dashboard/trips/[id]/page.tsx
@@ -1,9 +1,8 @@
-export default async function TripPage({ params }){
-    const { id } = await params;
+export default async function TripPage({ params }: { params: { id: string } }) {
+    const { id } = params;
     return (
         <div className="min-h-screen relative">
             <h1 className="text-3xl font-bold mb-6">Trip Details for Trip ID: {id}</h1>
         </div>
-    )
-
+    );
 }
