ca7be1c4fdbd9d29a56b2d1d50b74dc2df6fd566
Nikita Bryndak <nikitabryndak@gmail.com>
2026-02-07 17:49:54 +0000

    refactor: remove trustHost option from authOptions in route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -69,7 +69,6 @@ export const authOptions: AuthOptions = {
     },
     secret: process.env.NEXTAUTH_SECRET,
     debug: process.env.NODE_ENV === "development",
-    trustHost: true,
     pages: {
         signIn: "/login",
         error: "/login"
