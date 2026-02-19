9256e557e8a12b1ada020a13947be7431a791d38
Nikita Bryndak <nikitabryndak@gmail.com>
2026-02-19 11:05:46 +0000

    feat: update input component placeholder opacity for improved visibility

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -8,11 +8,11 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type,
 		<input
 			type={type}
 			className={cn(
-				"peer flex h-10 w-full rounded-md border border-white bg-background px-3 py-2 text-sm text-white placeholder-white focus:outline-none focus:border-accent focus:ring-2 focus:ring-accent focus:ring-offset-1 focus:ring-offset-white transition",
+				"peer flex h-10 w-full rounded-md border border-white bg-background px-3 py-2 text-sm text-white placeholder-white/40 focus:outline-none focus:border-accent focus:ring-2 focus:ring-accent focus:ring-offset-1 focus:ring-offset-white transition",
 				className
 			)}
 
-			
+
 			ref={ref}
 			{...props}
 		/>
